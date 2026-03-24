685f72aeb1f0f5bfbe3571b9ff902c559356bd2a
RaginiSharma01 <raginisharma.r07@gmail.com>
2026-03-24 13:53:53 +0530

    commits

--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -2,22 +2,35 @@ package middleware
 
 import (
 	"ems/config"
+	"ems/utils"
+	"strings"
 
 	"github.com/gofiber/fiber/v3"
 )
 
 func AuthMiddleware(c fiber.Ctx) error {
 
-	token := c.Get("Authorization")
+	authHeader := c.Get("Authorization")
 
-	if token == "" {
+	if authHeader == "" {
 		return c.Status(401).JSON(fiber.Map{
-			"message": "unauthorized",
+			"message": "authorization header missing",
 		})
 	}
 
-	val, err := config.RedisClient.Get(config.Ctx, token).Result()
+	// Extract token
+	token := strings.TrimPrefix(authHeader, "Bearer ")
 
+	// Verify JWT
+	_, err := utils.VerifyJWT(token)
+	if err != nil {
+		return c.Status(401).JSON(fiber.Map{
+			"message": "invalid token",
+		})
+	}
+
+	// Check Redis blacklist
+	val, err := config.RedisClient.Get(config.Ctx, token).Result()
 	if err == nil && val == "blacklisted" {
 		return c.Status(401).JSON(fiber.Map{
 			"message": "token invalid, login again",
--- utils/jwt.go
+++ utils/jwt.go
@@ -1,10 +1,13 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt"
 )
+
+var jwtSecret = []byte("secret_key")
 
 func GenerateJWT(empID string, email string) (string, error) {
 	claims := jwt.MapClaims{
@@ -15,9 +18,34 @@ func GenerateJWT(empID string, email string) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	tokenString, err := token.SignedString([]byte("your_secret_key"))
+	tokenString, err := token.SignedString([]byte("secret_key"))
 	if err != nil {
 		return "", err
 	}
 	return tokenString, nil
+}
+
+func VerifyJWT(tokenString string) (jwt.MapClaims, error) {
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+
+		// check signing method
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+
+		return jwtSecret, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+
+	if !ok || !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+	return claims, nil
 }
